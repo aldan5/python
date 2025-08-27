1e22989628f523e93d16fc73e7781e13250fc20a
aldan5 <aldansyukran5@gmail.com>
2025-08-27 15:53:45 +0800

    sa cape

---
+++ copian.py
@@ -0,0 +1,40 @@
+print("--------------------")
+print("--Program La_Kasir--")
+print("--------------------")
+tot = 0
+while True:
+    bg = input("masukkan Barang         : ")   
+    jm = int(input("Berapa Pcs          : "))
+    hr = int(input("masukkan Harga      :"))
+    hasil = jm*hr
+    tot+=hasil
+    print(f'{tot:,}')
+    qw = input("apakah masih ada? : ")
+    if(qw == 'lanjut'):
+        print(f"brg : {bg}")
+        print(f"pcs : {jm}")
+        print(f"price : {hr}")
+        print(f"total sebesar Rp.{tot:,}")
+    elif(qw == 'stop'): 
+        print(f"brg : {bg}")
+        print(f"pcs : {jm}")
+        print(f"price : {hr}")
+        print(f"total sebesar Rp.{tot:,}")
+        break
+    else:
+        print("nama tidak valid")
+
+
+
+
+
+   
+
+
+
+    
+
+    
+
+
+   
