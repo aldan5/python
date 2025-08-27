ac07cf1d24d5c17863659327a77c0cb108003254
aldan5 <aldansyukran5@gmail.com>
2025-08-27 14:24:59 +0800

    mulai ber table

--- kasir.py
+++ kasir.py
@@ -3,9 +3,9 @@ print("--Program La_Kasir--")
 print("--------------------")
 tot = 0
 while True:
-    bg = input("masukkan Barang ")   
-    jm = int(input("Berapa Pcs "))
-    hr = int(input("masukkan Harga "))
+    bg = input("masukkan Barang         : ")   
+    jm = int(input("Berapa Pcs          : "))
+    hr = int(input("masukkan Harga      :"))
     hasil = jm*hr
     tot+=hasil
     print(f'{tot:,}')
@@ -16,13 +16,17 @@ while True:
         print(f"price : {hr}")
         print(f"total sebesar Rp.{tot:,}")
         
-    /*masukkan tabel untuk total belanjaan*/
+    # *masukkan tabel untuk total belanjaan*
     elif(qw == 'stop'): 
         print(f"brg : {bg}")
         print(f"pcs : {jm}")
         print(f"price : {hr}")
         print(f"total sebesar Rp.{tot:,}")
         break
+    else:
+        print("nama tidak valid")
+    
+print(f"nama")
    
 
 
