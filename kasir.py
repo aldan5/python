8cc618d67d45defc0ea3ccdfa476bf3f7ebad1f6
aldan5 <aldansyukran5@gmail.com>
2025-08-27 14:32:17 +0800

    tabel : Punya head tabel

--- kasir.py
+++ kasir.py
@@ -1,32 +1,38 @@
-print("--------------------")
-print("--Program La_Kasir--")
-print("--------------------")
-tot = 0
-while True:
-    bg = input("masukkan Barang         : ")   
-    jm = int(input("Berapa Pcs          : "))
-    hr = int(input("masukkan Harga      :"))
-    hasil = jm*hr
-    tot+=hasil
-    print(f'{tot:,}')
-    qw = input("apakah masih ada? : ")
-    if(qw == 'lanjut'):
-        print(f"brg : {bg}")
-        print(f"pcs : {jm}")
-        print(f"price : {hr}")
-        print(f"total sebesar Rp.{tot:,}")
+# print("--------------------")
+# print("--Program La_Kasir--")
+# print("--------------------")
+# tot = 0
+# while True:
+#     bg = input("masukkan Barang         : ")   
+#     jm = int(input("Berapa Pcs          : "))
+#     hr = int(input("masukkan Harga      :"))
+#     hasil = jm*hr
+#     tot+=hasil
+#     print(f'{tot:,}')
+#     qw = input("apakah masih ada? : ")
+#     if(qw == 'lanjut'):
+#         print(f"brg : {bg}")
+#         print(f"pcs : {jm}")
+#         print(f"price : {hr}")
+#         print(f"total sebesar Rp.{tot:,}")
         
-    # *masukkan tabel untuk total belanjaan*
-    elif(qw == 'stop'): 
-        print(f"brg : {bg}")
-        print(f"pcs : {jm}")
-        print(f"price : {hr}")
-        print(f"total sebesar Rp.{tot:,}")
-        break
-    else:
-        print("nama tidak valid")
-    
-print(f"nama")
+#     # *masukkan tabel untuk total belanjaan*
+#     elif(qw == 'stop'): 
+#         print(f"brg : {bg}")
+#         print(f"pcs : {jm}")
+#         print(f"price : {hr}")
+#         print(f"total sebesar Rp.{tot:,}")
+#         break
+#     else:
+#         print("nama tidak valid")
+
+# table
+print("-" * 30)    
+print(f"{"barang":<10} {"jumlah":<10} {"harga":<5}")
+print("-" * 30)
+print(f"{"mangga":<10} {"4":^7} {"30000":>8}")
+
+
    
 
 
