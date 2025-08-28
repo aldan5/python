165aa7695514e20ff7d5105f80d2c4b59d70cbf9
aldan5 <aldansyukran5@gmail.com>
2025-08-28 10:56:49 +0800

    sudah bertabel

--- kasir.py
+++ kasir.py
@@ -1,16 +1,16 @@
+
 print("--------------------")
 print("--Program La_Kasir--")
 print("--------------------")
 tot = 0
-barang = []
-jumlah = []
-harga = []
+struks = []
 while True:
     bg = input("masukkan Barang         :     ")
     jm = int(input("Berapa Pcs          :     "))
     hr = int(input("masukkan Harga      :     "))
     hasil = jm*hr
     tot+=hasil
+    struks.append([bg,jm,hr])
     print(f"total {hasil:,}")
     print(f'Total Keseluruhan {tot:,}')
     qw = input("apakah masih ada? : ")
@@ -18,10 +18,14 @@ while True:
         print(f"Tot : {hasil:,}")
         print(f"total sebesar Rp.{tot:,}")
     elif(qw == 'stop'):
-        print("---STRUK PEMBAYARAN-----")
-        print("-" * 30)    
-        print(f"{"barang":<10} {"jumlah":<10} {"harga":<5}")
+        print("      ")
+        print("-" * 30)   
+        print("-------STRUK PEMBAYARAN-------")
         print("-" * 30)
+        print(f"{"barang":<10} {"jumlah":<10} {"harga":<5} {"total":<10}")
+        print("-" * 30)
+        print(f"{bg:<10} {jm:<10} {hr:<5}")
         break
     else:
         print("nama tidak valid")
+
