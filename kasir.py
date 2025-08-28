0330f6f24da101885b2f9ca67aa5f59ff50296e3
aldan5 <aldansyukran5@gmail.com>
2025-08-28 10:33:37 +0800

    fix awal

--- kasir.py
+++ kasir.py
@@ -2,8 +2,11 @@ print("--------------------")
 print("--Program La_Kasir--")
 print("--------------------")
 tot = 0
+barang = []
+jumlah = []
+harga = []
 while True:
-    bg = input("masukkan Barang         :     ")   
+    bg = input("masukkan Barang         :     ")
     jm = int(input("Berapa Pcs          :     "))
     hr = int(input("masukkan Harga      :     "))
     hasil = jm*hr
@@ -14,23 +17,11 @@ while True:
     if(qw == 'lanjut'):
         print(f"Tot : {hasil:,}")
         print(f"total sebesar Rp.{tot:,}")
-    elif(qw == 'stop'): 
-        print(f"total sebesar Rp.{tot:,}")
+    elif(qw == 'stop'):
+        print("---STRUK PEMBAYARAN-----")
+        print("-" * 30)    
+        print(f"{"barang":<10} {"jumlah":<10} {"harga":<5}")
+        print("-" * 30)
         break
     else:
         print("nama tidak valid")
-
-
-
-
-
-   
-
-
-
-    
-
-    
-
-
-   
