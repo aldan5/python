a26a42bdef792a3575182bdb4c85c3cd105669b4
aldan5 <aldansyukran5@gmail.com>
2025-08-28 09:40:29 +0800

    add tot seluruh

--- kasir.py
+++ kasir.py
@@ -3,22 +3,18 @@ print("--Program La_Kasir--")
 print("--------------------")
 tot = 0
 while True:
-    bg = input("masukkan Barang         : ")   
-    jm = int(input("Berapa Pcs          : "))
-    hr = int(input("masukkan Harga      :"))
+    bg = input("masukkan Barang         :     ")   
+    jm = int(input("Berapa Pcs          :     "))
+    hr = int(input("masukkan Harga      :     "))
     hasil = jm*hr
     tot+=hasil
-    print(f'{tot:,}')
+    print(f"total {hasil:,}")
+    print(f'Total Keseluruhan {tot:,}')
     qw = input("apakah masih ada? : ")
     if(qw == 'lanjut'):
-        print(f"brg : {bg}")
-        print(f"pcs : {jm}")
-        print(f"price : {hr}")
+        print(f"Tot : {hasil:,}")
         print(f"total sebesar Rp.{tot:,}")
     elif(qw == 'stop'): 
-        print(f"brg : {bg}")
-        print(f"pcs : {jm}")
-        print(f"price : {hr}")
         print(f"total sebesar Rp.{tot:,}")
         break
     else:
